5db6c0224e3e96248e8a3236bac07227b838d820
TheSpecTroll <92375017+TheSpecTroll@users.noreply.github.com>
2024-02-27 15:07:12 +0100

    Committing target folder

---
+++ demo/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.example.demo","c":"DemoApplication","l":"DemoApplication()","u":"%3Cinit%3E()"},{"p":"com.example.demo","c":"DemoApplication","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
