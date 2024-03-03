9b28f557b3cbe829c2a6f17fee8d3a7d8ca868f6
TheSpecTroll <92375017+TheSpecTroll@users.noreply.github.com>
2024-03-03 19:56:40 +0100

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,8 +6,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 @Test
+    public void testAdd() {
+        calcule math = new calcule ();
+        int result = math.add(3, 4);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void testMulti() {
+        calcule math = new calcule ();
+        int result = math.multi(3, 4);
+        assertEquals(12, result);
+    }
+
+    @Test
+    public void testDiv() {
+        calcule math = new calcule ();
+        int result = math.div(10, 2);
+        assertEquals(5, result);
+    }
+}
 
 }
